5050c9a7bf59942ae6973e61112cdcdc32f28bd4
hoaht-8203 <110768345+hoaht-8203@users.noreply.github.com>
2022-11-10 14:01:05 +0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,48 @@
+package BookStore;
+
+public class Book {
+	private String ID;
+	private String name;
+	private double price;
+	
+	public Book(String ID, String name, double price) {
+		this.setID(ID);
+		this.setName(name);
+		this.setPrice(price);
+		
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public void showInfo() {
+		System.out.println("Book Info:");
+		System.out.println("- Name \t\t: " + this.getName());
+		System.out.println("- ID \t\t: " + this.getID());
+		System.out.println("- Price \t: " + this.getPrice());
+	}
+	
+	
+	
+}
